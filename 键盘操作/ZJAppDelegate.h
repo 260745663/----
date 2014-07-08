711945b11a2d56adfd27936383a597f83009ef9a
260745663 <gkios2@demaxiya.local>
2014-07-08 20:03:15 +0800

    Revert 648f3bc..38913e9
    This rolls back to commit 648f3bcb5f8fe151742d57cd381c52c24bed47e2.

---
+++ 键盘操作/ZJAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  ZJAppDelegate.h
+//  键盘操作
+//
+//  Created by Fuck you on 14-6-30.
+//  Copyright (c) 2014年 ZJ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ZJAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ 键盘操作/ZJViewController.h
@@ -0,0 +1,14 @@
+//
+//  ZJViewController.h
+//  键盘操作
+//
+//  Created by Fuck you on 14-6-30.
+//  Copyright (c) 2014年 ZJ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ZJViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UITextField *TextField;
+
+@end
